c993252ceba885601f211b7a001a043484f3ec17
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-11-27 20:17:11 +0600

    Loading a specific kind of Codable data

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Hafizur Rahman on 27/11/25.
+//
+
+import SwiftUI
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
