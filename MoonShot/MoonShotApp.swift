30e4e73755c4e9bec5fc1e45e04ac87e37f620ae
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-11-27 06:38:58 +0600

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Hafizur Rahman on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
