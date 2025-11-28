ba89d856e61fb5240a5398465f6d627a294a628b
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-11-28 09:47:43 +0600

    ContentView Complete

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
     let columns = [
-        GridItem(.adaptive(minimum: 150)),
+        GridItem(.adaptive(minimum: 150), spacing: 12),
     ]
     
     var body: some View {
@@ -28,23 +28,37 @@ struct ContentView: View {
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
+                                    .padding()
                                 
                                 VStack {
                                     Text(mission.displayName)
                                         .font(.headline)
                                         .fontDesign(.rounded)
+                                        .foregroundStyle(.white)
                                     
                                     Text(mission.formattedLaunchDate)
                                         .font(.caption)
                                         .fontDesign(.rounded)
+                                        .foregroundStyle(.white.opacity(0.5))
                                 }
+                                .padding(.vertical)
                                 .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
                             }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay (
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
                         }
+                        .padding(.bottom, 4)
                     }
                 }
+                .padding([.horizontal, .bottom])
             }
+            .background(.darkBackground)
             .navigationTitle("Moonshot")
+            .preferredColorScheme(.dark)
         }
     }
 }
